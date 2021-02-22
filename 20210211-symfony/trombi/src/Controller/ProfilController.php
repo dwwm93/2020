37a0de237acdc17bebdd5f71dc026a31498d0b2e
dwwm93 <74361568+dwwm93@users.noreply.github.com>
2021-02-22 16:59:40 +0100

    Ajoute les formulaires dans symfony

--- 20210211-symfony/trombi/src/Controller/ProfilController.php
+++ 20210211-symfony/trombi/src/Controller/ProfilController.php
@@ -3,8 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Profil;
+use App\Form\Type\ProfilType;
 use App\Repository\ProfilRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,7 +14,7 @@ class ProfilController extends AbstractController
 {
     /**
      * "READ" la collection => la liste de tous les profils
-     * 
+     *
      * @Route("/profil", name="profil")
      */
     public function index(): Response
@@ -39,6 +41,121 @@ class ProfilController extends AbstractController
 
         return $this->render("profil/show.html.twig", [
             'p' => $profil,
+        ]);
+    }
+
+    /**
+     * "CREATE" un nouvel item
+     * 1er appel: "$_GET" affichage du formulaire
+     * 2ème appel : "$_POST" => "manager"
+     *
+     * @Route("/profil/new", name="profil_create")
+     */
+    public function create(Request $request)
+    {
+        $profil = new Profil;
+        /**
+         * Form = c'est un formulaire "php"
+         * FormBuilder = c'est un "constructeur" de Form "php"
+         */
+        $form = $this->createForm(ProfilType::class, $profil);
+
+        /**
+         * Récupère le "POST" de l'utilisateur dans le Form,
+         * en meme temps, "Form" remplit l'entité $profil
+         */
+        $form->handleRequest($request);
+        
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($profil);
+            // $profil->getId() == null
+            $em->flush();
+
+            return $this->redirectToRoute("profil_show", [
+                "id" => $profil->getId()
+            ]);
+        }
+
+        return $this->render('profil/new.html.twig', [
+            "formulaire" => $form->createView(),
+        ]);
+    }
+
+    /**
+     * "UPDATE" un item existant
+     * 1er appel: "$_GET" affichage du formulaire
+     * 2ème appel : "$_POST" => "manager"
+     *
+     * ParamConverter va transformer tout seul l'id en Profil !!
+     *
+     * @Route("/profil/{id}/update", name="profil_update")
+     */
+    public function update(
+        Profil $profil,
+        Request $request,
+        EntityManagerInterface $manager
+    ) {
+        /**
+         * Form = c'est un formulaire "php"
+         * FormBuilder = c'est un "constructeur" de Form "php"
+         */
+        $form = $this->createForm(ProfilType::class, $profil);
+
+        /**
+         * Récupère le "POST" de l'utilisateur dans le Form,
+         * en meme temps, "Form" remplit l'entité $profil
+         */
+        $form->handleRequest($request);
+        
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager->flush();
+
+            return $this->redirectToRoute("profil_show", [
+                "id" => $profil->getId()
+            ]);
+        }
+
+        return $this->render('profil/update.html.twig', [
+            "formulaire" => $form->createView(),
+        ]);
+    }
+
+    /**
+     * "DELETE" un item existant
+     * 1er appel: "$_GET" affichage du formulaire
+     * 2ème appel : "$_POST" => "manager"
+     *
+     * ParamConverter va transformer tout seul l'id en Profil !!
+     *
+     * @Route("/profil/{id}/delete", name="profil_delete")
+     */
+    public function delete(
+        Profil $profil,
+        Request $request,
+        EntityManagerInterface $manager
+    ) {
+        $form = $this->createFormBuilder($profil)
+            ->add("delete", SubmitType::class, ['attr' => [
+                "class" => "btn btn-error"
+            ]])
+            ->getForm();
+
+        /**
+         * Récupère le "POST" de l'utilisateur dans le Form,
+         * en meme temps, "Form" remplit l'entité $profil
+         */
+        $form->handleRequest($request);
+        
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager->delete($profil);
+            $manager->flush();
+
+            return $this->redirectToRoute("profil");
+        }
+
+        return $this->render('profil/delete.html.twig', [
+            "formulaire" => $form->createView(),
         ]);
     }
 }
---
+++ 20210211-symfony/trombi/src/Form/Type/ProfilType.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+class ProfilType extends AbstractType {
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom', TextType::class)
+            ->add('prenom', TextType::class)
+            ->add('dateNaissance', DateType::class)
+            ->add('avatar', TextType::class)
+        ;
+    }
+}
